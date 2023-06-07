0821ef5e851b1ac4549d6c97e58feacb9084b869
gigas <refyangigas471@gmail.com>
2023-06-07 20:30:22 +0700

    penambahan API login,register,logout

--- routes/api.php
+++ routes/api.php
@@ -1,26 +1,11 @@
 <?php
 
+use App\Http\Controllers\AndroiAuthController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\SubcategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::group([
-//     'middleware' => 'api',
-//     'prefix' => 'auth'
-// ], function(){
-//     Route::get('login', [AuthController::class,'login'])->name('login');
-//     Route::post('login', [AuthController::class,'login'])->name('login');
-// });
-
-// Route::group([
-//     'middleware'=>'api'
-// ],
-
-// function(){
-//     Route::resources([
-//        'categories'=>CategoryController::class,
-//        'subcategories'=>subcategoryController::class
-//     ]);
-// });
+Route::post('/login', [AndroiAuthController::class, 'login']);
+Route::post('/register', [AndroiAuthController::class, 'register']);
