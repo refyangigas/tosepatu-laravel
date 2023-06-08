330293c8c7dddf2ced7e75f1654322e401fc5c88
Akbar Kusnadi ( Akkusna ) <120556264+Akkusna@users.noreply.github.com>
2023-06-09 03:05:38 +0700

    Update backend dashboard #2

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Layanan;
+use App\Models\Transaksi;
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -9,7 +12,16 @@ class DashboardController extends Controller
 {
     public function index(Request $request)
     {
+        $datapendapatan = Transaksi::where('status','selesai')->whereMonth('tanggal', date('m'))->whereYear('tanggal', date('Y'))->sum('total');
+        $datatransaksi = Transaksi::whereMonth('tanggal', date('m'))->whereYear('tanggal', date('Y'))->count();
+        $user = User::count();
+        $datalayanan = Layanan::count();
         // dd(Auth::user());
-        return view('admin.pages.dashboard');
+        return view('admin.pages.dashboard',[
+        'totalpendapatan' => $datapendapatan,
+        'totaltransaksi' => $datatransaksi,
+        'totaldatauser' => $user,
+        'totaldatalayanan' => $datalayanan,
+    ]);
     }
 }
--- resources/views/admin/pages/dashboard.blade.php
+++ resources/views/admin/pages/dashboard.blade.php
@@ -18,11 +18,9 @@
                     <div class="card-body">
                         <div class="row align-items-center">
                             <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-uppercase mb-1">Earnings (Monthly)</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                                <div class="text-xs font-weight-bold text-uppercase mb-1">Pendapatan (Bulan)</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">Rp. {{number_format($totalpendapatan)}}</div>
                                 <div class="mt-2 mb-0 text-muted text-xs">
-                                    <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                                    <span>Since last month</span>
                                 </div>
                             </div>
                             <div class="col-auto">
@@ -38,11 +36,9 @@
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-uppercase mb-1">Sales</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">650</div>
+                                <div class="text-xs font-weight-bold text-uppercase mb-1">Transaksi (Bulan)</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{$totaltransaksi}}</div>
                                 <div class="mt-2 mb-0 text-muted text-xs">
-                                    <span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 12%</span>
-                                    <span>Since last years</span>
                                 </div>
                             </div>
                             <div class="col-auto">
@@ -58,11 +54,9 @@
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-uppercase mb-1">New User</div>
-                                <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">366</div>
+                                <div class="text-xs font-weight-bold text-uppercase mb-1"> Total User</div>
+                                <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{$totaldatauser}}</div>
                                 <div class="mt-2 mb-0 text-muted text-xs">
-                                    <span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 20.4%</span>
-                                    <span>Since last month</span>
                                 </div>
                             </div>
                             <div class="col-auto">
@@ -78,11 +72,9 @@
                     <div class="card-body">
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-uppercase mb-1">Pending Requests</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                                <div class="text-xs font-weight-bold text-uppercase mb-1">Jumlah Layanan</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{$totaldatalayanan}}</div>
                                 <div class="mt-2 mb-0 text-muted text-xs">
-                                    <span class="text-danger mr-2"><i class="fas fa-arrow-down"></i> 1.10%</span>
-                                    <span>Since yesterday</span>
                                 </div>
                             </div>
                             <div class="col-auto">
@@ -97,7 +89,7 @@
             <div class="col-xl-8 col-lg-7">
                 <div class="card mb-4">
                     <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                        <h6 class="m-0 font-weight-bold text-primary">Monthly Recap Report</h6>
+                        <h6 class="m-0 font-weight-bold text-primary">Grafik Pendapatan (Hari Ini)</h6>
                         <div class="dropdown no-arrow">
                             <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -124,7 +116,7 @@
             <div class="col-xl-4 col-lg-5">
                 <div class="card mb-4">
                     <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                        <h6 class="m-0 font-weight-bold text-primary">Products Sold</h6>
+                        <h6 class="m-0 font-weight-bold text-primary">Jumlah Orderan (Bulan)</h6>
                         <div class="dropdown no-arrow">
                             <a class="dropdown-toggle btn btn-primary btn-sm" href="#" role="button"
                                 id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -132,7 +124,7 @@
                             </a>
                             <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
                                 aria-labelledby="dropdownMenuLink">
-                                <div class="dropdown-header">Select Periode</div>
+                                <div class="dropdown-header">Select Period</div>
                                 <a class="dropdown-item" href="#">Today</a>
                                 <a class="dropdown-item" href="#">Week</a>
                                 <a class="dropdown-item active" href="#">Month</a>
@@ -141,50 +133,8 @@
                         </div>
                     </div>
                     <div class="card-body">
-                        <div class="mb-3">
-                            <div class="small text-gray-500">Oblong T-Shirt
-                                <div class="small float-right"><b>600 of 800 Items</b></div>
-                            </div>
-                            <div class="progress" style="height: 12px;">
-                                <div class="progress-bar bg-warning" role="progressbar" style="width: 80%"
-                                    aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                        </div>
-                        <div class="mb-3">
-                            <div class="small text-gray-500">Gundam 90'Editions
-                                <div class="small float-right"><b>500 of 800 Items</b></div>
-                            </div>
-                            <div class="progress" style="height: 12px;">
-                                <div class="progress-bar bg-success" role="progressbar" style="width: 70%"
-                                    aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                        </div>
-                        <div class="mb-3">
-                            <div class="small text-gray-500">Rounded Hat
-                                <div class="small float-right"><b>455 of 800 Items</b></div>
-                            </div>
-                            <div class="progress" style="height: 12px;">
-                                <div class="progress-bar bg-danger" role="progressbar" style="width: 55%"
-                                    aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                        </div>
-                        <div class="mb-3">
-                            <div class="small text-gray-500">Indomie Goreng
-                                <div class="small float-right"><b>400 of 800 Items</b></div>
-                            </div>
-                            <div class="progress" style="height: 12px;">
-                                <div class="progress-bar bg-info" role="progressbar" style="width: 50%"
-                                    aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
-                        </div>
-                        <div class="mb-3">
-                            <div class="small text-gray-500">Remote Control Car Racing
-                                <div class="small float-right"><b>200 of 800 Items</b></div>
-                            </div>
-                            <div class="progress" style="height: 12px;">
-                                <div class="progress-bar bg-success" role="progressbar" style="width: 30%"
-                                    aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
-                            </div>
+                        <div class="chart-container" style="position: relative; height: 260px;">
+                            <canvas id="orderChart"></canvas>
                         </div>
                     </div>
                     <div class="card-footer text-center">
@@ -193,6 +143,47 @@
                     </div>
                 </div>
             </div>
+            <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+            <script>
+                document.addEventListener("DOMContentLoaded", function(event) {
+                    // Data untuk grafik
+                    var data = {
+                        labels: ["January", "February", "March", "April", "May", "June"],
+                        datasets: [{
+                            label: "Order Quantity",
+                            data: [600, 500, 455, 400, 200],
+                            backgroundColor: "rgba(54, 162, 235, 0.2)",
+                            borderColor: "rgba(54, 162, 235, 1)",
+                            borderWidth: 1,
+                            fill: true,
+                        }]
+                    };
+            
+                    // Opsi konfigurasi grafik
+                    var options = {
+                        responsive: true,
+                        maintainAspectRatio: false,
+                        tooltips: {
+                            callbacks: {
+                                title: function(tooltipItem, data) {
+                                    return 'Month: ' + data.labels[tooltipItem[0].index];
+                                },
+                                label: function(tooltipItem, data) {
+                                    return 'Orders: ' + tooltipItem.yLabel;
+                                }
+                            }
+                        }
+                    };
+            
+                    // Membuat grafik menggunakan Chart.js
+                    var ctx = document.getElementById('orderChart').getContext('2d');
+                    var orderChart = new Chart(ctx, {
+                        type: 'line',
+                        data: data,
+                        options: options
+                    });
+                });
+            </script>
             <!-- Invoice Example -->
             <div class="col-xl-8 col-lg-7 mb-4">
                 <div class="card">
