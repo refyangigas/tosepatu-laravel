3729c4526911e55559dac35e55d6a6f14f2f9ef4
refyangigas <refyangigas471@gmail.com>
2023-06-11 00:30:11 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,7 @@
 <?php
 
 use App\Http\Controllers\AndroiAuthController;
-use App\Http\Controllers\AndroidApi;
 use App\Http\Controllers\AndroidApiController;
-use App\Http\Controllers\api\ApiLayananController;
-use App\Http\Controllers\api\ApiPembayaranController;
-use App\Http\Controllers\api\ApiPengirimanController;
-use App\Http\Controllers\api\ApiPenjemputanController;
-use App\Http\Controllers\api\ApiTransaksiController;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\SubcategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
