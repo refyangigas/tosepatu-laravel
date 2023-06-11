dbcbd1b914a94f1fcd441b59204781eecc19ca2a
AdityaPur <120556278+AdityaPur@users.noreply.github.com>
2023-06-11 20:57:26 +0700

    Update laporan.blade.php

--- resources/views/admin/pages/laporan.blade.php
+++ resources/views/admin/pages/laporan.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.main')
 
 @section('content')
+<div class="container-fluid" id="container-wrapper">
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Laporan</h1>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="dashboard">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Laporan</li>
+            </ol>
+        </div>
  <!-- Datatables -->
  <div class="col-lg-12">
   <div class="card mb-4">
